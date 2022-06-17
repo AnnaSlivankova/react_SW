8fede3f900df178c2fd4e38dfaa73d972569f0b2
Anna Slivankova <annslivankova@gmail.com>
2022-06-17 17:29:36 +0300

    third commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import c from './Header.module.css';
+
+const Header = () => {
+    return (
+        <header className={c.header}>
+            <img src='https://klike.net/uploads/posts/2020-04/1587719791_1.jpg'/>
+        </header>
+    );
+}
+
+export default Header;
