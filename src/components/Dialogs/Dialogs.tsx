9e701f9d233d86781961a62ebbd8b80de3fd3271
Anna Slivankova <annslivankova@gmail.com>
2022-10-25 14:05:10 +0300

    added css styling  (SW 21)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import c from './Dialogs.module.css';
+import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog + ' ' + s.active}>Anna</div>
+                <div className={s.dialog}>Alex</div>
+                <div className={s.dialog}>Ron</div>
+                <div className={s.dialog}>Helga</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi! When do you come to as?</div>
+                <div className={s.message}>Nice to meet you!</div>
+                <div className={s.message}>Call you boss! Immediately...he is so angry</div>
+                <div className={s.message}>You need this car)) look...</div>
+            </div>
         </div>
     );
 }
