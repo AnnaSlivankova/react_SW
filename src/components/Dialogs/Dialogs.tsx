1e2230ac9152359dde9177c00efd1ad79873f1f2
Anna Slivankova <annslivankova@gmail.com>
2022-10-25 17:47:00 +0300

    added props for Dialogs and some css styling  (SW 23)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
+
+type DialogsItemType = {
+    id: string
+    name: string
+}
+type DialogMessageType = {
+    message: string
+}
+
+const DialogsItems = (props: DialogsItemType) => {
+    return <div className={s.dialog + ' ' + s.active}>
+        <NavLink to={"/dialogs/" + props.id}>{props.name}</NavLink>
+    </div>
+}
+const DialogMessage = (props: DialogMessageType) => {
+    return <div className={s.message}>{props.message}</div>
+}
+
 
 const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogItems}>
-                <div className={s.dialog + ' ' + s.active}>Anna</div>
-                <div className={s.dialog}>Alex</div>
-                <div className={s.dialog}>Ron</div>
-                <div className={s.dialog}>Helga</div>
+                <DialogsItems id="anna" name="Anna"/>
+                <DialogsItems id="alex" name="Alex"/>
+                <DialogsItems id="ron" name="Ron"/>
+                <DialogsItems id="helga" name="Helga"/>
             </div>
             <div className={s.messages}>
-                <div className={s.message}>Hi! When do you come to as?</div>
-                <div className={s.message}>Nice to meet you!</div>
-                <div className={s.message}>Call you boss! Immediately...he is so angry</div>
-                <div className={s.message}>You need this car)) look...</div>
+                <DialogMessage message={"Hi! When do you come to as?"}/>
+                <DialogMessage message={"Nice to meet you!"}/>
+                <DialogMessage message={"Call you boss! Immediately...he is so angry"}/>
+                <DialogMessage message={"You need this car)) look..."}/>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import b from './MyPosts.module.css';
+import s from './MyPosts.module.css';
 import Post from "./Posts/Post";
 
 const MyPosts = () => {
     return (
         <div>
             My posts
-            <div>
-                <textarea></textarea>
-                <button>Add new post</button>
+            <div className={s.AddPostItem}>
+                <div>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <button>Add new post</button>
+                </div>
             </div>
-            <div className={b.content}>
+
+            <div className={s.content}>
                 <Post message={"This is my first message"}
                       like={1}/>
                 <Post message={"I bought a dag"}
